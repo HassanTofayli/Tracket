d6969fe86fffacb01cc962d91c4d0edf90824750
HassanTofayli <85042863+HassanTofayli@users.noreply.github.com>
2023-06-23 15:06:05 +0300

    Update Tracket_main.ino

--- Tracket_main.ino
+++ Tracket_main.ino
@@ -5,6 +5,7 @@
 #include <LittleFS.h>
 #include <ArduinoJson.h>
 #include <time.h>
+#include <HardwareSerial.h>
 
 #define FORMAT_LITTLEFS_IF_FAILED true
 
@@ -13,7 +14,7 @@ byte daysavetime = 1;
 
 const char* config_filename = "/config.json";
 const int serialNumber = 123456;
-String name;
+String name = "DefaultVehcileName";
 String type;
 int batterySize;
 int memorySize;
@@ -29,10 +30,12 @@ String api_dev_key = "nbDlIM7wkLevjB6-wdT__xI2nSHhLnhW";
 String api_user_name = "HassanTofayli";
 String api_user_password = "Tracket.vehiclecontrolsystem";
 String vehiclePasteKey = "";
-
+String api_user_key;
+const char* serverUrl = "http://192.168.1.109:3000/esp";
 WiFiClient wifiClient;
 HTTPClient httpClient;
-
+HardwareSerial SerialGPS(2);   // Define the serial port to use for the GPS module
+char gpsData[256]; // Declare a character array to store GPS data
 
 void writeFile(String filename, String message) {
   File file = LittleFS.open(filename, "w");
@@ -238,25 +241,14 @@ String httpGETRequest(const char* serverName) {
 }
 
 String getPrivatePaste(String url) {
-  
   HTTPClient http;
-  http.begin("https://pastebin.com/api/api_login.php");
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  http.POST("api_dev_key="+api_dev_key+"&api_user_name="+api_user_name+"&api_user_password="+api_user_password);
-
-  // Check the response
-  String response = http.getString();
-  if (response.startsWith("Bad API request")) {
-    Serial.println("Login failed");
-    return "Login failed" + response;
+  if (api_user_key == "NoKey") {
+    Serial.println("No User Key Found To Get the Private Paste");
+    return "false";
   }
-
-  http.end();
-
-  // Send another HTTP request to get the private paste
   http.begin("https://pastebin.com/api/api_raw.php");
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  http.POST("api_dev_key="+api_dev_key+"&api_user_key=" + response + "&api_option=show_paste&api_paste_key="+configPasteKey);
+  http.POST("api_dev_key=" + api_dev_key + "&api_user_key=" + api_user_key + "&api_option=show_paste&api_paste_key=" + configPasteKey);
 
   // Check the response
   String payload = http.getString();
@@ -264,18 +256,17 @@ String getPrivatePaste(String url) {
   http.end();
   return payload;
 }
-
 String configFetching() {
 
   String configJson = getPrivatePaste(configPasteLink);
   if (configJson != "false") {
     configFetchingSucc = true;
     Serial.println("Config File: " + configJson);
-      DynamicJsonDocument doc(1024);
-      deserializeJson(doc, configJson);
-      String outputString;
-      serializeJson(doc, outputString);
-      Serial.println(outputString);
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, configJson);
+    String outputString;
+    serializeJson(doc, outputString);
+    Serial.println(outputString);
     vehiclePasteKey = searchJson(outputString);
     Serial.println(vehiclePasteKey);
   } else {
@@ -284,7 +275,6 @@ String configFetching() {
   }
   return configJson;
 }
-
 String searchJson(String jsonContent) {
   DynamicJsonDocument doc(1024);
   deserializeJson(doc, jsonContent);
@@ -295,18 +285,82 @@ String searchJson(String jsonContent) {
   if (obj.containsKey(key)) {
     Serial.print("Found key: ");
     Serial.println(key);
-    return obj[key].as<String>(); // Return the value associated with the key
+    return obj[key].as<String>();  // Return the value associated with the key
   }
 
-  return "false"; // Return "false" if the key is not found
+  return "false";  // Return "false" if the key is not found
 }
+String createNewPaste() {
+  String url = "https://pastebin.com/api/api_post.php";
+  String postData = "api_option=paste&api_user_key=" + api_user_key + "&api_paste_private=2&api_paste_name=My%20Private%20Paste&api_paste_expire_date=N&api_dev_key=" + String(api_dev_key) + "&api_paste_code=This%20is%20my%20private%20paste%20content.";
+  httpClient.begin(url);
+  httpClient.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpCode = httpClient.POST(postData);
+  String response = httpClient.getString();
+  httpClient.end();
+  if (httpCode == HTTP_CODE_OK) {
+    Serial.println("Paste created. URL: " + response);
+  } else {
+    Serial.println("Error creating paste");
+  }
+}
+String getUserApiKey() {
+
+  HTTPClient http;
+  http.begin("https://pastebin.com/api/api_login.php");
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  http.POST("api_dev_key=" + api_dev_key + "&api_user_name=" + api_user_name + "&api_user_password=" + api_user_password);
+
+  // Check the response
+  String response = http.getString();
+  if (response.startsWith("Bad API request")) {
+    Serial.println("Login failed: " + response);
+    return "NoKey";
+  }
+
+  http.end();
+  Serial.println("getUserApiKey: " + response);
+  return response;
+}
+
+void sendTextData( String data) {
+  HTTPClient http;
+  String serverUrl = "http://192.168.1.109:3000/esp";
+  http.begin(serverUrl);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  http.addHeader("X-Device", "ESP32");
+  http.addHeader("X-Sensor", "DHT11");
+  http.addHeader("X-Temp", "25");
+  http.addHeader("X-Humidity", "50");
+
+  StaticJsonDocument<200> jsonDoc;
+  jsonDoc["message"] = data;
+  jsonDoc["timestamp"] = millis();
+  String requestBody;
+  serializeJson(jsonDoc, requestBody);
+  int httpResponse = http.POST(requestBody);
+  if (httpResponse > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponse);
+    String response = http.getString();
+    Serial.println(response);
+  } else {
+    Serial.print("HTTP Error code: ");
+    Serial.println(httpResponse);
+  }
+  http.end();
+}
+
 
 
 
 void setup() {
   Serial.begin(115200);
+  SerialGPS.begin(9600, SERIAL_8N1, 3, 1); // Initialize the GPS module serial port
   wm.setConfigPortalBlocking(false);
+  wm.setTimeout(30);
   wm.autoConnect("ESP32-HassanTofayli");
+
 
 
   if (!LittleFS.begin(false)) {
@@ -333,13 +387,33 @@ void setup() {
     }
   }
   Serial.println("My serialNumber is: " + String(serialNumber));
-  configFetching();
+  api_user_key = getUserApiKey();
+  // Serial.println(api_user_key);
+  // if(api_user_key!="NoKey"){
+  //   configFetching();
+  // createNewPaste();
+  // }
+
+  // // Get user key
+  // HTTPClient http;
+  // String url = "https://pastebin.com/api/api_login.php";
+  // String postData = "api_option=login&api_user_name=" + String(api_user_name) + "&api_user_password=" + String(api_user_password) + "&api_dev_key=" + String(api_dev_key);
+  // http.begin(url);
+  // http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  // int httpCode = http.POST(postData);
+  // String response = http.getString();
+  // http.end();
+  // if (httpCode == HTTP_CODE_OK) {
+  //   String api_user_key = response;
+  //   Serial.println("User key retrieved: " + api_user_key);
+
+  // Create private paste
 }
 
 void loop() {
 
   if (WiFi.isConnected() || WiFi.status() != WL_CONNECTED) {
-
+    // wm.stopConfigPortal();
     Serial.print("WiFi connected: ");
     Serial.println(WiFi.localIP());
   } else {
@@ -351,7 +425,24 @@ void loop() {
       wm.stopConfigPortal();
     }
   }
-
+  if (SerialGPS.available()) { // Check if GPS data is available
+    Serial.write(SerialGPS.read()); // Echo GPS data to the Serial Monitor
+  }
+String gpsData = ""; // Declare a string variable to store GPS data
+  
+  // Wait for GPS data to become available
+  while (SerialGPS.available() > 0) {
+    char c = SerialGPS.read(); // Read character from GPS module
+    gpsData += c; // Append character to gpsData string
+    delay(1);
+  }
+  
+  if (gpsData.length() > 0) {
+    // Print GPS data to Serial Monitor
+    Serial.println(gpsData);
+  }
+  String dataToSend = gpsData;
+  sendTextData(dataToSend);
 
   delay(5000);
 }
